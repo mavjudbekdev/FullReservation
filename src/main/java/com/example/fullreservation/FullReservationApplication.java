bf335efe82981d6591642e5b83f130ea9f4af4d3
mavjudbek <mavjudbeksokhibov@gmail.com>
2023-12-21 15:14:58 +0500

    commit push

---
+++ src/main/java/com/example/fullreservation/FullReservationApplication.java
@@ -0,0 +1,13 @@
+package com.example.fullreservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullReservationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FullReservationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fullreservation/FullReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fullreservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullReservationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
